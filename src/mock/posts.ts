e5cce32dda3a73e5cffc114acefef480afec1c5b
nicolasbispo <nbm.personalwork@gmail.com>
2024-03-25 18:41:10 -0300

    added: mocks to categories, navbarlinks and posts

---
+++ src/mock/posts.ts
@@ -0,0 +1,38 @@
+export const lastPosts = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor sit amet",
+    postThumbImage: "https://source.unsplash.com/random",
+    created_at: new Date(),
+    updated_at: new Date()
+    
+  },
+  {
+    id: 2,
+    title: "Consectetur adipiscing elit",
+    postThumbImage: "https://source.unsplash.com/random",
+    created_at: new Date(),
+    updated_at: new Date()
+  },
+  {
+    id: 3,
+    title: "Sed do eiusmod tempor incididunt",
+    postThumbImage: "https://source.unsplash.com/random",
+    created_at: new Date(),
+    updated_at: new Date()
+  },
+  {
+    id: 4,
+    title: "Ut labore et dolore magna aliqua",
+    postThumbImage: "https://source.unsplash.com/random",
+    created_at: new Date(),
+    updated_at: new Date()
+   },
+  {
+    id: 5,
+    title: "Excepteur sint occaecat cupidatat non proident",
+    postThumbImage: "https://source.unsplash.com/random",
+    created_at: new Date(),
+    updated_at: new Date()
+  }
+];
