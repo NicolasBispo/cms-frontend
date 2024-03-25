25d32b4c05263d540f0173e72e94613e50cf026c
nicolasbispo <nbm.personalwork@gmail.com>
2024-03-25 18:40:41 -0300

    added: date helper

---
+++ src/helpers/date/index.ts
@@ -0,0 +1,10 @@
+import dayjs from "dayjs";
+import locale from "dayjs/locale/pt-br" 
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+export function humanizeDate(date: Date) {
+  dayjs.locale(locale)
+  dayjs.extend(relativeTime)
+  const formattedDate = dayjs(date);
+  return formattedDate.fromNow();
+}
